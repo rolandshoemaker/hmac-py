07d14373b2494d4ddecab563531a004b08922a84
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-08-04 03:55:19 +0000

    first commit, v0.1

---
+++ hmacBasic.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# hmacBasic.py v0.1
+# basic implementation of hmac for server<->server communication for flask
+
+import time
+import hmac
+import json
+import requests
+from flask import request, url_for, Flask
+
+# sign message
+def send_hmac(*arg): # send_hmac(url, json_data)
+	# our shared private key
+	secret = arg[0].config['SECRET_KEY']
+	# what time is it? (unix seconds from epoch)
+	timenow = time.time()
+
+	# sign JSON message
+	if len(arg) > 3:
+		digester = hmac.new(secret)
+		# the blob we use here is the path of the request that we want to make, the json data, and the clients timestamp
+		digester.update(arg[1]+str(arg[3])+str(timenow))
+		digest = digester.hexdigest()
+
+		# and send, including the hash and timestamp so that the hash can be recalculated later
+		headers = {'Content-Type': 'application/json'}
+		r = requests.post(arg[1]+arg[2]+'?timestamp='+str(timenow)+'&hash='+digest, data=json.dumps(arg[3]), headers=headers)
+
+		return r.json()
+	# sign url message
+	else:
+		digester = hmac.new(secret)
+        	digester.update(arg[2]+str(timenow))
+        	digest = digester.hexdigest()
+
+		r = requests.get(arg[1]+arg[2]+'?timestamp='+str(timenow)+'&hash='+digest)
+
+		return r.json()
+		
+# verify message
+def authorize_hmac(app):
+    	# the shared private key that is used between api and client, this implementation doesn't use public keys to identify users since there is only one user...
+	secret = app.config['SECRET_KEY']
+	# make sure we were sent the client timestamp and the HMAC hash
+	if request.args.get('hash') and request.args.get('timestamp'):
+		client_hash = request.args.get('hash')
+		# get the timestamp given to us by the client
+		client_timestamp = request.args.get('timestamp')
+
+	        # check that the clients timestamp hasn't expired (timestamp+10 minutes), this helps thwart timing attacks
+		if time.time() < float(client_timestamp)+float(600):
+	        	# verify json message
+			if request.json:
+				digester = hmac.new(secret)
+				# the blob we use here is the path of the request that was made, the json data, and the clients timestamp
+				digester.update(request.path+str(request.json)+str(client_timestamp))
+				digest = digester.hexdigest()
+				# true if they match, false if they don't
+				return digest == client_hash
+			# verify url var message
+			else:
+				digester = hmac.new(secret)
+				# since all the message payload is in the url we only need to blob the request path and client timestamp
+				digester.update(request.path+str(client_timestamp))
+				digest = digester.hexdigest()
+				return digest == client_hash
+		else:
+			return False
+	else:
+		return False
